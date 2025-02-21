bc14925218e77b77d98c3254669dc36fba7b5d2b
Abi-de-jo <abish@example.com>
2025-02-21 21:20:35 +0530

    done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -39,19 +39,10 @@ export default function Home() {
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          {/* <Search />
-          <CategoryFilter /> */}
+         
         </div>
 
-        {/* <Collection 
-          data={events?.data}
-          emptyTitle="No Events Found"
-          emptyStateSubtext="Come back later"
-          collectionType="All_Events"
-          limit={6}
-          page={page}
-          totalPages={events?.totalPages}
-        /> */}
+       
       </section>
 
 
